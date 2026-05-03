81ca57cb16889b0bbed9273d92e4c7a6ad3f6645
Teddy112345 <aleksandrhlupin102@gmail.com>
2026-05-03 13:21:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from pygame import *
+from random import randint
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_y, player_x, player_speed, size):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), size)
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.y = player_y
+        self.rect.x = player_x
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_d] and self.rect.x < 700 - self.rect.width - 5:
+            self.rect.x += self.speed
+        if keys[K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+    def fire(self):
+        pass
+
+window = display.set_mode((700, 500))
+display.set_caption('Шутер')
+
+clock = time.Clock()
+FPS = 60
+run = True
+finish = False
+
+font.init()
+my_font = font.SysFont("Arial", 35)
+win = my_font.render("Вы победили!", True, (255, 255, 0))
+
+while run:
+    for e  in event.get():
+        if e.type == QUIT:
+            run = False
+
+    if not finish:
+        pass
+    display.update() 
+    clock.tick(60)
